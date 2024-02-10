7925adf936f036d80cc4942df9398d39bc349a11
Jeffrey Cheung <CHEUNG.K.Jeffrey@gmail.com>
2024-02-10 13:52:24 -0800

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -1,20 +1,84 @@
-let body = document.querySelector('body');
+let body = document.querySelector("body");
 let today = new Date();
 let thisYear = today.getFullYear();
-let footer = document.createElement('footer');
-let copyright = document.createElement('p');
+let footer = document.createElement("footer");
+let copyright = document.createElement("p");
 
 copyright.innerHTML = `\u00A9 ${thisYear} Jeffrey Cheung`;
 footer.appendChild(copyright);
 
-let skills = [ "HTML", "CSS", "JavaScript"];
+let skills = ["HTML", "CSS", "JavaScript"];
 let skillsSections = document.querySelector("#Skills");
 let skillsList = document.querySelector("#Skills > ul");
 
-for(let i = 0; i < skills.length; i++) {
+for (let i = 0; i < skills.length; i++) {
     let skill = document.createElement("li");
     skill.innerHTML = skills[i];
     skillsList.appendChild(skill);
 }
 
 body.append(footer);
+
+let messageForm = document.querySelector("form[name='leave_message']");
+messageForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let name = event.target.usersName.value;
+    let email = event.target.usersEmail.value;
+    let message = event.target.usersMessage.value;
+    console.log(`${name} ${email} ${message}`);
+
+    let messageSection = document.querySelector("section#messages");
+    let messageList = document.querySelector("section#messages > ul");
+    let newMessage = document.createElement("li");
+    newMessage.innerHTML = `<a href="mailto:${email}">${name}</a><span>${message}</span>`;
+    let removeButton = document.createElement("button");
+    removeButton.innerText = "remove";
+    removeButton.type = "button";
+    removeButton.addEventListener("click", event => {
+        let entry = event.target.parentNode;
+        entry.remove();
+    })
+    let editButton = document.createElement("button");
+    editButton.innerText = "edit";
+    let editBox;
+    editButton.addEventListener("click", event => {
+        let entry = event.target.parentNode;
+        if (!editBox) {
+            editBox = document.createElement("textarea");
+            editBox.value = entry.childNodes[1].innerText;
+            entry.appendChild(editBox);
+            editButton.innerText = "save";
+            return;
+        }
+        entry.childNodes[1].innerText = entry.lastChild.value;
+        entry.lastChild.remove();
+        editBox = undefined;
+        editButton.innerText = "edit";
+    })
+    newMessage.appendChild(editButton);
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+    event.target.reset();
+})
+
+let messageList = document.querySelector("section#messages > ul");
+
+let onMessageListChange = (mutationList, observer) => {
+    for (const mutation of mutationList) {
+        if (mutation.type === "childList"){
+            if(mutation.target.hasChildNodes()) {
+                mutation.target.parentNode.style = "display: block;";
+            } else {
+                mutation.target.parentNode.style = "display: none;";
+            }
+        }
+    }
+}
+
+let observer = new MutationObserver(onMessageListChange);
+
+observer.observe(messageList, {childList: true});
+
+// Initialize the message section
+onMessageListChange([{target: messageList, type: "childList"}]);
+
